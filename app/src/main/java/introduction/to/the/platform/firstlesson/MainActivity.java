859e6c2beac08495a03d62bb7d9535b3d6052933
anka6136@gmail.com <kofiac8BE9f2>
2021-11-17 18:53:12 +0300

    Новое активити для переключения тем

--- app/src/main/java/introduction/to/the/platform/firstlesson/MainActivity.java
+++ app/src/main/java/introduction/to/the/platform/firstlesson/MainActivity.java
@@ -2,31 +2,26 @@ package introduction.to.the.platform.firstlesson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-
-import com.google.android.material.radiobutton.MaterialRadioButton;
 
 public class MainActivity extends AppCompatActivity {
     private TextView displayTextView;
-    private static final int AppThemeCoolCodeStyle = 0;
-    private static final int AppThemeLightCodeStyle = 1;
-    private static final int AppThemeDarkCodeStyle = 2;
+    public static final String NameSharedPreference = "LOGIN";
     private static final String AppTheme = "APP_THEME";
-    private static final String NameSharedPreference = "LOGIN";
 
     Calc calc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setTheme(getAppTheme(R.style.MyCoolStyle));
-        setContentView(R.layout.activity_main2);
 
-        initThemeChooser();
+        super.onCreate(savedInstanceState);
+        ChooseThemeActivity chooseThemeActivity = new ChooseThemeActivity();
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
+        setTheme(chooseThemeActivity.codeStyleToStyleResId(sharedPref.getInt(AppTheme, 0)));
+        setContentView(R.layout.activity_main2);
 
         displayTextView = findViewById(R.id.textViewDisplayText);
 
@@ -62,52 +57,11 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.buttonResult).setOnClickListener(v -> {
             displayTextView.setText(calc.result(displayTextView.getText().toString()));
         });
-    }
 
-    private void initThemeChooser() {
-        initRadioButton(findViewById(R.id.radioButtonCoolTheme),
-                AppThemeCoolCodeStyle);
-        initRadioButton(findViewById(R.id.radioButtonLightTheme),
-                AppThemeLightCodeStyle);
-        initRadioButton(findViewById(R.id.radioButtonDarkTheme),
-                AppThemeDarkCodeStyle);
-        RadioGroup rg = findViewById(R.id.radioButtonsGroupChooseTheme);
-        ((MaterialRadioButton)rg.getChildAt(getCodeStyle(AppThemeCoolCodeStyle))).setChecked(true);
-        // - При первом запуске падает, но я не смогла понять почему он не нашел чайлда радиогруппы по стилю
-    }
-    private void initRadioButton(View button, final int codeStyle){
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setAppTheme(codeStyle);
-                recreate();
-            }
+        findViewById(R.id.buttonThemesActivity).setOnClickListener(v -> {
+            Intent intent = new Intent(this, ChooseThemeActivity.class);
+
+            startActivity(intent);
         });
-    }
-
-    private int getAppTheme(int codeStyle) {
-        return codeStyleToStyleId(getCodeStyle(codeStyle));
-    }
-
-    private void setAppTheme(int codeStyle) {
-       SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
-       SharedPreferences.Editor editor = sharedPref.edit();
-       editor.putInt(AppTheme, codeStyle);
-       editor.apply();
-   }
-
-    private int codeStyleToStyleId(int codeStyle){
-        switch (codeStyle){
-            case AppThemeLightCodeStyle:
-                return R.style.MyLightStyle;
-            case AppThemeDarkCodeStyle:
-                return R.style.MyDarkStyle;
-            default: return R.style.MyCoolStyle;
-        }
-    }
-
-    private int getCodeStyle(int codeStyle){
-        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
-        return sharedPref.getInt(AppTheme, codeStyle);
     }
 }
